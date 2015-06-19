08f77c620c42161e70e062d03d0c0336e07e737b
Jon Spencer <jon@jonspencer.ca>
2015-06-18 21:45:15 -0700

    add icon to the file list - ditch select element

--- app/IndexView.js
+++ app/IndexView.js
@@ -6,40 +6,112 @@ let filesListHbs = require('./files-list');
 let commitMessageHbs = require('./commit-message');
 let app = require('./app');
 
+function patchPath(patch) {
+	return patch.newFile || patch.oldFile;
+}
+
 let MultiFilesView = Backbone.View.extend({
 
 	initialize: function() {
 		this.listenTo(this.collection, "all", this.render);
 		this.listenTo(app.repoSettings, "change:focusFiles", this.setSelected);
+		this.el.classList.add("multi-file-view");
+		this.focus = -1;
+		this.origin = -1;
 		return this.render();
 	},
 
 	events: {
-		"change select": "onChange",
+		"click .file-item": "onClick",
+		'keydown .file-list': 'onKeyPress',
 	},
 
-	onChange: function(ev) {
-		var focusFiles = {};
-		focusFiles[this.key] = this.$("option:selected").map(function() { return this.value; }).get();
-		app.repoSettings.set("focusFiles", focusFiles);
+	onKeyPress: function(ev) {
+		if (ev.keyCode !== 40 && ev.keyCode !== 38)
+			return;
+
+		let items = this.$(".file-item");
+		if (items.length === 0) return;
+
+		let n = this.focus, p = n;
+		if (n < 0 || n >= items.length)
+			p = n = 0;
+		else if (ev.keyCode === 40 && this.focus + 1 < items.length)
+			n = this.focus + 1;
+		else if (ev.keyCode === 38 && this.focus > 0)
+			n = this.focus - 1;
+
+		if (n === this.focus)
+			return;
+		this.focus = n;
+
+		if (ev.shiftKey) {
+			if (this.origin === -1)
+				this.origin = p;
+
+			let f = [], a = {};
+			for (let i = Math.min(n, this.origin); i <= Math.max(n, this.origin); i += 1) {
+				let r = this.collection.get(items[i].id);
+				f.push(patchPath(r.get('patch')));
+			}
+			a[this.key] = f;
+			app.repoSettings.set("focusFiles", a);
+		}
+		else {
+			this.origin = -1;
+			let r = this.collection.get(items[this.focus].id), a = {};
+			a[this.key] = [ patchPath(r.get('patch')) ];
+			app.repoSettings.set("focusFiles", a);
+		}
+	},
+
+	onClick: function(ev) {
+		ev.stopPropagation();
+		ev.preventDefault();
+
+		this.focus = $(ev.currentTarget).index();
+
+		let r = this.collection.get(ev.currentTarget.id);
+		let path = patchPath(r.get("patch"));
+
+		let a = app.repoSettings.get("focusFiles");
+		let selected = (a && a[this.key]) || [];
+
+		if (ev.ctrlKey) {
+			selected = selected.concat([]);
+			let i = selected.indexOf(path);
+			if (i === -1)
+				selected.push(path);
+			else
+				selected.splice(i, 1);
+		}
+		else {
+			selected = [ path ];
+		}
+
+		a = {};
+		a[this.key] = selected;
+		app.repoSettings.set("focusFiles", a);
 	},
 
 	setSelected: function() {
-		var a = app.repoSettings.get("focusFiles");
-		var selected = (a && a[this.key]) || [];
-		this.$("option").each(function() {
-			$(this).prop("selected", selected.indexOf(this.value) !== -1);
+		let c = this.collection;
+		let a = app.repoSettings.get("focusFiles");
+		let selected = (a && a[this.key]) || [];
+		this.$(".file-item").each(function() {
+			let r = c.get(this.id);
+			let on = selected.indexOf(patchPath(r.get("patch"))) !== -1;
+			if (on) this.classList.add("selected");
+			else this.classList.remove("selected");
 		});
 	},
 
 	record: function() {
 		return {
 			files: this.collection.map(function(r) {
-				let patch = r.get("patch");
-				let oldFile = patch.oldFile;
-				let newFile = patch.newFile;
 				return {
-					path: newFile || oldFile
+					id: r.cid,
+					path: patchPath(r.get("patch")),
 				};
 			}),
 		};
@@ -48,9 +120,10 @@ let MultiFilesView = Backbone.View.extend({
 	render: function() {
 		var record = this.record();
 		this.$el.html(filesListHbs(record));
-		this.$("select").addClass(this.key);
 		this.$(".index-header").text(this.title);
+
 		this.setSelected();
+
 		return this;
 	},
 });
