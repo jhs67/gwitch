3574bdee6bd7dd45ae0375c23d1ac7666ede8b42
Jon Spencer <jon@jonspencer.ca>
2016-01-12 21:17:41 -0800

    create user interface to select blocks of diff lines

--- app/PickView.js
+++ app/PickView.js
@@ -1,9 +1,90 @@
 "use strict";
 
+var $ = require('jquery');
 var Backbone = require("backbone");
 var DiffView = require("./DiffView");
 
 var PickView = DiffView.extend({
+
+	events: {
+		"mousedown .diff-line": "dragStart",
+		"click .button": "buttonClick",
+	},
+
+	dragStart: function(ev) {
+		if (ev.button !== 0)
+			return;
+		ev.preventDefault();
+
+		this.lines = this.$('.diff-line').toArray();
+		this.anchor = this.lines.indexOf(ev.currentTarget);
+		this.lines.forEach(function(e) { e.classList.remove('selected'); });
+		this.last = this.anchor;
+
+		// Use the window events to get a little more range.
+		$(window).on('mousemove.pick', this.dragMove.bind(this));
+		$(window).one('mouseup', this.dragEnd.bind(this));
+		ev.currentTarget.classList.add('selected');
+
+		var b = this.$('.button');
+		b.removeClass('show');
+	},
+
+	dragMove: function(ev) {
+		var es = this.lines;
+		var l = 0, h = es.length;
+		while (h - l > 1) {
+			var m = Math.floor((l + h) / 2);
+			var em = es[m];
+			if (ev.clientY <= $(em).offset().top)
+				h = m;
+			else
+				l = m;
+		}
+
+		if (l < this.anchor) {
+			for (h = l; h < this.anchor && h < this.last; h += 1)
+				es[h].classList.add('selected');
+			for (h = this.last; h < l; h += 1)
+				es[h].classList.remove('selected');
+			for (h = this.last; h > this.anchor; h -= 1)
+				es[h].classList.remove('selected');
+		}
+		else {
+			for (h = l; h > this.anchor && h > this.last; h -= 1)
+				es[h].classList.add('selected');
+			for (h = this.last; h < this.anchor; h += 1)
+				es[h].classList.remove('selected');
+			for (h = this.last; h > l; h -= 1)
+				es[h].classList.remove('selected');
+		}
+		this.last = l;
+	},
+
+	dragEnd: function(ev) {
+		$(window).off('mousemove.pick');
+
+		var es = this.lines;
+		var l = this.anchor;
+		while (l > 0 && es[l - 1].classList.contains('selected'))
+			l -= 1;
+
+		var top = 1;
+		for (var e = es[l]; e && !e.classList.contains('patches'); e = e.parentElement)
+			top += e.offsetTop;
+
+		var b = this.$('.stage.button');
+		b.css('top', top);
+		b.addClass('show');
+
+		var text = this.settings.get("focusFiles") && this.settings.get("focusFiles").staged ? "unstage" : "stage";
+		text += (this.last === this.anchor) ? " line" : " lines";
+		b.text(text);
+	},
+
+	buttonClick: function(ev) {
+		console.log("buttonClick");
+	},
 
 	initialize: function(opt) {
 		this.settings = opt.settings;
@@ -14,6 +95,8 @@ var PickView = DiffView.extend({
 	},
 
 	record: function() {
+		this.$('.button').removeClass('show');
+
 		var patches = [];
 		var focusFiles = this.settings.get("focusFiles");
 		var staged = (focusFiles && focusFiles.staged) || [];
