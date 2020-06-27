81372e4f2701cb31ac1d455b0cf4eb0a1613c14b
Jon Spencer <jon@jonspencer.ca>
2020-06-27 11:37:52 -0700

    install react and convert the renderer to jsx

---
+++ src/renderer.tsx
@@ -0,0 +1,5 @@
+import './index.css'
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>hello world from gwitch! </div>, document.getElementById('root'));
