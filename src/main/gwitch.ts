8a4585fe5f10e1ab16c5e7546a45a27cd10eab10
Jon Spencer <jon@jonspencer.ca>
2020-06-27 23:59:57 -0700

    resurrect the window size management code

--- src/main/gwitch.ts
+++ src/main/gwitch.ts
@@ -1,26 +1,23 @@
 import { RecentStore } from "./recent-store";
 import { BrowserWindow, dialog } from "electron";
+import { WindowManager } from "./window-manager";
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 
 export default class Gwitch {
   private recent = new RecentStore();
+  private windows = new WindowManager();
 
   async init(): Promise<void> {
-    await this.recent.load();
+    await Promise.all([await this.recent.load(), await this.windows.load()]);
     this.createWindow();
   }
 
   createWindow(): void {
-    const windowOpts: Electron.BrowserWindowConstructorOptions = {
-      height: 600,
-      width: 800,
-      webPreferences: {
-        nodeIntegration: true,
-      },
-    };
+    const windowOpts = this.windows.opts({ webPreferences: { nodeIntegration: true } });
 
     // Create the browser window.
     const window = new BrowserWindow(windowOpts);
+    this.windows.track(window);
 
     // and load the index.html of the app.
     window.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
---
+++ src/main/window-manager.ts
@@ -0,0 +1,108 @@
+import {
+  screen,
+  BrowserWindow,
+  Rectangle,
+  app,
+  BrowserWindowConstructorOptions,
+} from "electron";
+import { promises as fs } from "fs";
+import { join } from "path";
+import deepEqual from "deep-equal";
+
+const FileName = "window-state-n.json";
+
+interface WindowState {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  isMaximized: boolean;
+  isFullScreen: boolean;
+  displayBounds: Rectangle;
+}
+
+function isValidWindowState(body: WindowState) {
+  if (typeof body !== "object") return;
+  if (typeof body.x !== "number" || typeof body.y !== "number") return false;
+  if (typeof body.width !== "number" || typeof body.height !== "number") return false;
+  if (typeof body.isMaximized !== "boolean") return false;
+  if (typeof body.isFullScreen !== "boolean") return false;
+
+  const displayBounds = screen.getDisplayMatching(body).bounds;
+  if (!deepEqual(body.displayBounds, displayBounds, { strict: true })) return false;
+
+  return true;
+}
+
+export class WindowManager {
+  private windows = new Map();
+  private state?: WindowState;
+
+  async load() {
+    try {
+      const file = join(app.getPath("userData"), FileName);
+      const body = JSON.parse(await fs.readFile(file, "utf8"));
+      if (!isValidWindowState(body)) throw new Error("loaded window state no longer valid");
+      this.state = body;
+    } catch (err) {
+      console.info("error loading window state", err);
+      this.state = undefined;
+    }
+  }
+
+  loadState(window: BrowserWindow) {
+    const winBounds = window.getBounds();
+    this.state = {
+      isMaximized: window.isMaximized(),
+      isFullScreen: window.isFullScreen(),
+      displayBounds: screen.getDisplayMatching(winBounds).bounds,
+      x: winBounds.x,
+      y: winBounds.y,
+      width: winBounds.width,
+      height: winBounds.height,
+    };
+  }
+
+  async save() {
+    const file = join(app.getPath("userData"), FileName);
+    await fs.writeFile(file, JSON.stringify(this.state), "utf8");
+  }
+
+  track(window: BrowserWindow) {
+    const id = window.id;
+    this.windows.set(id, window);
+
+    if (this.state?.isMaximized) window.maximize();
+    if (this.state?.isFullScreen) window.setFullScreen(true);
+
+    window.on("close", () => {
+      this.loadState(window);
+    });
+
+    window.on("closed", async () => {
+      this.windows.delete(id);
+      await this.save();
+    });
+  }
+
+  opts(base: BrowserWindowConstructorOptions = {}): BrowserWindowConstructorOptions {
+    const r: BrowserWindowConstructorOptions = {
+      width: 1200,
+      height: 800,
+      ...base,
+    };
+
+    if (this.state) {
+      r.width = this.state.width;
+      r.height = this.state.height;
+
+      // For the first window only, set the position
+      if (this.windows.keys().next().done) {
+        r.x = this.state.x;
+        r.y = this.state.y;
+      }
+    }
+
+    return r;
+  }
+}
