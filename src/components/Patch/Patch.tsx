6b4fac2d5d981043cfc45539ce377b111d5a5968
Jon Spencer <jon@jonspencer.ca>
2020-08-11 14:22:10 -0700

    show the patch for the selected stage files

--- src/components/Patch/Patch.tsx
+++ src/components/Patch/Patch.tsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { createUseStyles } from "react-jss";
+import { RootState } from "../../store";
+import { Diff } from "../Diff";
+
+const useStyles = createUseStyles({
+  patchContainer: {
+    position: "absolute",
+    height: "100%",
+    width: "100%",
+    overflow: "auto",
+    padding: 5,
+  },
+});
 
 export function Patch() {
-  return <div>patch</div>;
+  const workingStatus = useSelector((state: RootState) => state.repo.workingStatus);
+  const indexStatus = useSelector((state: RootState) => state.repo.indexStatus);
+  const workingSelected = useSelector((state: RootState) => state.repo.workingSelected);
+  const indexSelected = useSelector((state: RootState) => state.repo.indexSelected);
+  const classes = useStyles();
+
+  const [status, selected] = indexSelected?.length
+    ? [indexStatus || [], indexSelected]
+    : [workingStatus || [], workingSelected];
+
+  const patch = status.filter((s) =>
+    selected?.length ? selected.indexOf(s.newFile || s.oldFile) !== -1 : true,
+  );
+
+  return (
+    <div className={classes.patchContainer}>
+      <Diff
+        patch={patch}
+        diffLimit={400}
+        addLimit={50}
+        show={{}}
+        setShow={() => undefined}
+      />
+    </div>
+  );
 }
